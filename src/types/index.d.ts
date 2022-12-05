8fddd25c27b57f641fd9748e441c59836d237800
Vicky <hwfongfing@gmail.com>
2022-12-06 00:26:22 +0900

    feat: add d.ts for image & default.css file

---
+++ src/types/index.d.ts
@@ -0,0 +1,21 @@
+﻿declare module '*.svg' {
+  import React = require('react');
+
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const value: any;
+  export default value;
+}
+declare module '*.gif' {
+  const value: any;
+  export default value;
+}
+
+// declare module '*.svg' {
+//   const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+//   export default value;
+// }
